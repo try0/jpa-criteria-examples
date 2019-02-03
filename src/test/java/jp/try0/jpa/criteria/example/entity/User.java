165a85b8f796b163395103b59b3f69782dd108b3
Ryo Tsunoda <try0.development@gmail.com>
2019-02-03 20:06:41 +0900

    User entity

--- src/test/java/jp/try0/jpa/criteria/example/entity/User.java
+++ src/test/java/jp/try0/jpa/criteria/example/entity/User.java
@@ -1,38 +1,37 @@
 package jp.try0.jpa.criteria.example.entity;
-import java.io.Serializable;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-/**
- * The persistent class for the user_account database table.
- *
- */
 @Entity
-@Table(name="user_account")
-@NamedQuery(name="UserAccount.findAll", query="SELECT u FROM UserAccount u")
-public class UserAccount implements Serializable {
+@Table(name = "user")
+@NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="user_id")
+	@Column(name = "user_id")
 	private String userId;
 
 	private String name;
 
 	private String password;
 
-	@Column(name="user_number")
+	@Column(name = "user_number")
 	private Integer userNumber;
 
+	@OneToMany(mappedBy = "user", cascade = { CascadeType.PERSIST })
+	private List<EMailAddress> eMailAddresses;
 
-
-
-
-	public UserAccount() {
+	public User() {
 	}
 
 	public String getUserId() {
